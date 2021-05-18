02b4e8222e0eb727bbb2d979470350093fe3e4a1
Nikhil Sriram <Nikhil.Sriram5@gmail.com>
2021-05-18 15:02:55 -0700

    setting up environment variables for easier deployment.

---
+++ config/<secretsConfig>.js
@@ -0,0 +1,3 @@
+module.exports.EDAMAM_URL = "https://api.edamam.com/api/nutrition-details";
+module.exports.EDAMAM_ID = "65e38df6";
+module.exports.EDAMAM_KEY = "3904fbeeec98b2ead00391cbd6952805";
---
+++ config/config.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+
+
+
+const configuration = {
+    local: {
+        databaseConfig: {
+            DATABASE_USERNAME: 'artain',
+            DATABSE_PASSWORD: '',
+            DATABASE_NAME: 'neighborhoodrecipe',
+            DATABSE_URL: '127.0.0.1',
+            DATABASE_PORT: '5432'
+        },
+        databaseOptions: {
+            "host": "127.0.0.1",
+            "dialect": "postgres",
+            "freezeTableName": true,
+            "port": 5432,
+            pool: {
+                acquire: 600000
+            },
+            "logging": false
+        }
+    },
+    aws: {
+        databaseConfig: {
+            DATABASE_USERNAME: process.env.DB_USERNAME || 'postgres',
+            DATABSE_PASSWORD: process.env.DB_PASSWORD,
+            DATABASE_NAME: process.env.DB_NAME,
+            DATABSE_URL: process.env.DB_URL, //'neighborhoodRecipe-dev.cyfaiieqbmoj.us-west-2.rds.amazonaws.com',
+            DATABASE_PORT: process.env.DB_PORT
+        },
+        databaseOptions: {
+            "host": process.env.DB_HOST, //"neighborhoodRecipe-dev.cyfaiieqbmoj.us-west-2.rds.amazonaws.com",
+            "dialect": "postgres",
+            "freezeTableName": true,
+            "port": process.env.DB_PORT,
+            pool: {
+                acquire: 600000
+            },
+            "logging": false
+        }
+    },
+    environmentOptions: {
+        environment: "LOCAL",
+    }
+}
+configuration.local.databaseConfig.DATABASE_NAME = (process.env.NODE_ENV === "test") ? "neighborhoodrecipetest" : "neighborhoodrecipe";
+
+module.exports.configuration = configuration;
+module.exports.BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+module.exports.SECRET_KEY = process.env.SECRET_KEY || "test";
+//module.exports.SESSION_SECRET = "overcooked";
+module.exports.EDAMAM_URL = process.env.EDAMAM_URL;
+module.exports.EDAMAM_ID = process.env.EDAMAM_ID;
+module.exports.EDAMAM_KEY = process.env.EDAMAM_KEY;
